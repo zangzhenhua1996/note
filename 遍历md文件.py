93dc63de4d0daad17c7cb0cc4a14bce0cde59676
zangzhenhua1996 <318977674@qq.com>
2020-01-08 11:23:55 +0800

    Deployed cff42c9 with MkDocs version: 1.0.4

--- 遍历md文件.py
+++ 遍历md文件.py
@@ -1,9 +1,9 @@
 import os
-path = r'mysql8-0使用'
+path = r'未分类'
 list1=[]
 for filename in os.listdir(path):
     if filename.endswith(".md"):
-        str1= '- '+ filename.replace(".md","") +': '+'mysql8-0使用/'+filename
+        str1= '- '+ filename.replace(".md","") +': '+path+'/'+filename
         list1.append(str1)
 data = list1
 data.reverse()
